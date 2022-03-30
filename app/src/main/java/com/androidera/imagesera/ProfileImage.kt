129e563ca5f26fa2bc3247a0fb18adb708d146d1
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-03-30 04:50:41 +0200

    Create gallery fragment class

--- app/src/main/java/com/androidera/imagesera/ProfileImage.kt
+++
@@ -1,7 +0,0 @@
-package com.androidera.imagesera
-
-data class ProfileImage(
-    val large: String,
-    val medium: String,
-    val small: String
-)
---
+++ app/src/main/java/com/androidera/imagesera/model/UnsplashPhoto.kt
@@ -0,0 +1,45 @@
+package com.androidera.imagesera.model
+
+import com.google.gson.annotations.SerializedName
+
+data class UnsplashPhoto(
+    @SerializedName("created_at")
+    val createdAt: String,
+    val description: String,
+    val height: Int,
+    val id: String,
+    val likes: Int,
+    val urls: Urls,
+    val user: User,
+    val width: Int
+)
+
+data class Urls(
+    val full: String,
+    val raw: String,
+    val regular: String,
+    val small: String,
+    val thumb: String
+)
+
+data class User(
+    @SerializedName("first_name")
+    val firstName: String,
+    val id: String,
+    @SerializedName("last_name")
+    val lastName: String,
+    val name: String,
+    @SerializedName("portfolio_url")
+    val portfolioUrl: String,
+    @SerializedName("profile_image")
+    val profileImage: ProfileImage,
+    val username: String
+)
+
+data class ProfileImage(
+    val large: String,
+    val medium: String,
+    val small: String
+)
+
+
