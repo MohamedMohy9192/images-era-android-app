89b8062e4672eea21a8502c4c516972f44a4ca8c
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-03-31 23:44:41 +0200

    Create retrofit service for unsplash API

---
+++ app/src/main/java/com/androidera/imagesera/network/UnsplashApiService.kt
@@ -0,0 +1,25 @@
+package com.androidera.imagesera.network
+
+import com.androidera.imagesera.BuildConfig
+import com.androidera.imagesera.model.UnsplashResponse
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+internal const val BASE_URL = "https://api.unsplash.com/"
+private const val UNSPLASH_ACCESS_KEY = BuildConfig.UNSPLASH_ACCESS_KEY
+
+interface UnsplashApiService {
+    @Headers(
+        "Accept-Version: v1",
+        "Authorization: Client-ID $UNSPLASH_ACCESS_KEY"
+    )
+    @GET("search/photos")
+    suspend fun searchPhotos(
+        @Query("query") searchQuery: String,
+        @Query("page") pageNumber: Int = 1,
+        @Query("per_page") numberOfItemsPerPage: Int = 20
+    ): UnsplashResponse
+}
