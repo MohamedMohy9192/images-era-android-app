856e6517636e5f3a5b920248185d6ad0bb6d1ffd
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-04-02 03:21:35 +0200

    Configure API paging source with unsplash repository

--- app/src/main/java/com/androidera/imagesera/UnsplashRepository.kt
+++ app/src/main/java/com/androidera/imagesera/UnsplashRepository.kt
@@ -1,5 +1,8 @@
 package com.androidera.imagesera
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.androidera.imagesera.network.UnsplashApiService
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -8,4 +11,14 @@ import javax.inject.Singleton
 class UnsplashRepository @Inject constructor(
     private val service: UnsplashApiService
 ) {
+
+    fun searchUnsplashPhotos(searchInput: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(service, searchInput) }
+        ).liveData
 }
